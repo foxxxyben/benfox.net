7ce6eb687c5e3a8ad5cef87d1eefe143516ca291
foxxxyben <32082323+foxxxyben@users.noreply.github.com>
2018-08-01 17:48:48 -0700

    wrong slick, slick

--- site/themes/carousel-slick/static/js/main.js
+++ site/themes/carousel-slick/static/js/main.js
@@ -1,5 +1,27 @@
 $(document).ready(function(){
-  $('.your-class').slick({
-    setting-name: setting-value
+  $('.carousel').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 1,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
   });
 });
